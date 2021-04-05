c0b2fe6cafe87374cc367e74c8126ed069abf1fb
Fábián Gábor <info@fabiangabor.com>
2021-04-05 20:48:36 +0200

    translationCutsModel

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import processing.data.Table;
 import processing.data.TableRow;
 
 public class Main extends PApplet {
+	final boolean DEBUG = true;
 	Table table;
 	boolean translate = false;
 	boolean rotate = false;
@@ -11,19 +12,125 @@ public class Main extends PApplet {
 	int translateX = 0;
 	int translateY = 0;
 	int countTranslateClicks = 0;
+	final boolean translationCutsModel = true;
+
+	final int BENT = 0;    // 0000
+	final int BAL = 1;    // 0001
+	final int JOBB = 2;    // 0010
+	final int LENT = 4;    // 0100
+	final int FENT = 8;    // 1000
+
+	float padding = 100;
+	float xMin;
+	float yMin;
+	float xMax;
+	float yMax;
+
+	static class Line {
+		float x1, y1, x2, y2;
+
+		public Line(float x1, float y1, float x2, float y2) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+		}
+	}
+
 
 	public void setup() {
 		size(640, 480);
 
+		xMin = padding;
+		yMin = padding;
+		xMax = width - xMin;
+		yMax = height - yMin;
+
 		table = new Table();
 		table.addColumn("x");
 		table.addColumn("y");
-		noLoop();
 	}
 
 	public void draw() {
 		background(204);
+
+		if (DEBUG)
+			rect(xMin, yMin, width - 2 * xMin, height - 2 * yMin);
+
 		drawLines(table);
+	}
+
+	int Zona(double x, double y) {
+		int zona = BENT;
+
+		if (x < xMin) zona |= BAL;
+		else if (x > xMax) zona |= JOBB;
+		if (y < yMin) zona |= LENT;
+		else if (y > yMax) zona |= FENT;
+
+		return zona;
+	}
+
+	Line CohenSutherlandSzakaszvago(float x0, float y0, float x1, float y1) {
+		int pont1Zona = Zona(x0, y0);
+		int pont2Zona = Zona(x1, y1);
+		boolean elfogad = false;
+		boolean vege = false;
+
+		do {
+			if ((pont1Zona | pont2Zona) == 0) {
+				// mindket pont bent van
+				elfogad = true;
+				vege = true;
+			} else {
+				if ((pont1Zona & pont2Zona) != 0) {
+					// mindket pont kozos kulso zonaban van (BAL, JOBB, FENT, LENT)
+					// nem fogadjuk el, vege
+					vege = true;
+				} else {
+					float x = 0, y = 0;
+
+					// egy pont biztosan kint van
+					int pontKint = Math.max(pont2Zona, pont1Zona);
+
+					// Metszespontok:
+					// m = (y1 - y0) / (x1 - x0)
+					// x = x0 + (1 / m) * (y[Min/Max] - y0)
+					// y = y0 + m * (x[Min/Max] - x0)
+					if ((pontKint & FENT) != 0) {
+						x = x0 + (x1 - x0) * (yMax - y0) / (y1 - y0);
+						y = yMax;
+					} else if ((pontKint & LENT) != 0) {
+						x = x0 + (x1 - x0) * (yMin - y0) / (y1 - y0);
+						y = yMin;
+					} else if ((pontKint & JOBB) != 0) {
+						y = y0 + (y1 - y0) * (xMax - x0) / (x1 - x0);
+						x = xMax;
+					} else if ((pontKint & BAL) != 0) {
+						y = y0 + (y1 - y0) * (xMin - x0) / (x1 - x0);
+						x = xMin;
+					}
+
+					if (pontKint == pont1Zona) {
+						x0 = x;
+						y0 = y;
+						pont1Zona = Zona(x0, y0);
+					} else {
+						x1 = x;
+						y1 = y;
+						pont2Zona = Zona(x1, y1);
+					}
+				}
+			}
+		}
+		while (!vege);
+
+
+		if (elfogad) {
+			drawLine(x0, y0, x1, y1);
+			return new Line(x0, y0, x1, y1);
+		}
+		return null;
 	}
 
 	void drawLine(float x1, float y1, float x2, float y2) {
@@ -63,31 +170,53 @@ public class Main extends PApplet {
 			x1 = table.getRow(i + 1).getInt("x");
 			y1 = table.getRow(i + 1).getInt("y");
 
-			drawLine(x1, y1, x2, y2);
+			//drawLine(x1, y1, x2, y2); // helyette CohenSutherlandSzakaszvago()
+			Line line = CohenSutherlandSzakaszvago(x1, y1, x2, y2);
+			if (translationCutsModel) {
+				if (line != null) {
+					x1 = (int) line.x1;
+					y1 = (int) line.y1;
+					x2 = (int) line.x2;
+					y2 = (int) line.y2;
+
+					table.getRow(i).setInt("x", x2);
+					table.getRow(i).setInt("y", y2);
+					table.getRow(i + 1).setInt("x", x1);
+					table.getRow(i + 1).setInt("y", y1);
+				} else {
+					table.removeRow(i + 1);
+					table.removeRow(i);
+				}
+			}
+
+			if (line != null)
+				drawLine(line.x1, line.y1, line.x2, line.y2);
 		}
 	}
 
 	public void mousePressed() {
-		if (!translate) {
+		if (translate || rotate || scale) {
+			if (translate) {
+				countTranslateClicks++;
+
+				if (countTranslateClicks % 2 == 0) {
+					translateX = mouseX - translateX;
+					translateY = mouseY - translateY;
+					countTranslateClicks = 0;
+
+					for (TableRow row : table.rows()) {
+						row.setInt("x", row.getInt("x") + translateX);
+						row.setInt("y", row.getInt("y") + translateY);
+					}
+				} else {
+					translateX = mouseX;
+					translateY = mouseY;
+				}
+			}
+		} else {
 			TableRow newRow = table.addRow();
 			newRow.setInt("x", mouseX);
 			newRow.setInt("y", mouseY);
-		} else {
-			countTranslateClicks++;
-
-			if (countTranslateClicks % 2 == 0) {
-				translateX = mouseX - translateX;
-				translateY = mouseY - translateY;
-				countTranslateClicks = 0;
-
-				for (TableRow row : table.rows()) {
-					row.setInt("x", row.getInt("x") + translateX);
-					row.setInt("y", row.getInt("y") + translateY);
-				}
-			} else {
-				translateX = mouseX;
-				translateY = mouseY;
-			}
 		}
 		redraw();
 	}
