a5f75d34b8f42d3625379cc0118fecd26a095d37
Fábián Gábor <info@fabiangabor.com>
2021-04-19 14:04:20 +0200

    kiiratas torolve

--- src/Main.java
+++ src/Main.java
@@ -152,8 +152,6 @@ public class Main extends PApplet {
 		float m;
 		float i, j;
 
-		println("Szakasz: (" + x1 + "," + y1 + "), (" + x2  + "," +  y2 + ")");
-
 		if (x2 != x1) { // nem függőleges
 			m = (y2 - y1) / (x2 - x1);
 
