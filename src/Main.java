1fa18a76c8876463cdef75172b4a2b8f45e4ca27
Fábián Gábor <info@fabiangabor.com>
2021-04-20 15:49:32 +0200

    fix rotate

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,28 @@ import processing.data.TableRow;
 import processing.event.MouseEvent;
 
 public class Main extends PApplet {
-	final boolean DEBUG = false;
 	Table table;
 	boolean translate = false;
 	boolean rotate = false;
-	int rotateAngle = 45;
+	int rotateAngle = 30;
 	boolean scale = false;
 	float transformX, transformY;
 	int countClicks = 0;
-	final boolean translationCutsModel = false;
 
-	final int BENT = 0;    // 0000
-	final int BAL = 1;    // 0001
-	final int JOBB = 2;    // 0010
-	final int LENT = 4;    // 0100
-	final int FENT = 8;    // 1000
+	final int width = 640;
+	final int height = 480;
+	final float originX = width / 3;
+	final float originY = height / 3;
 
-	float padding = 100;
-	float xMin;
-	float yMin;
-	float xMax;
-	float yMax;
+	final float[][] tInit = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+	float[] pInit = new float[]{0, 0, 1};
 
-	final float[][] tInit = new float[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-	final float[] pInit = new float[]{0, 0, 1};
+	final class Tinit {
+		final float[][] matrix = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+		Tinit() {
+		}
+	}
 
 
 	static class Line {
@@ -52,102 +50,27 @@ public class Main extends PApplet {
 
 
 	public void setup() {
-		size(640, 480);
-
-		if (DEBUG) {
-			xMin = padding;
-			yMin = padding;
-		} else {
-			xMin = 0;
-			yMin = 0;
-		}
-		xMax = width - xMin;
-		yMax = height - yMin;
+		size(width, height);
 
 		table = new Table();
 		table.addColumn("x");
 		table.addColumn("y");
+
+		TableRow newRow = table.addRow();
+		newRow.setFloat("x", 325);
+		newRow.setFloat("y", 245);
+
+		newRow = table.addRow();
+		newRow.setFloat("x", 340);
+		newRow.setFloat("y", 340);
 	}
 
 	public void draw() {
 		background(204);
+		line(0, originY, width, originY);
+		line(originX, 0, originX, height);
 
-		checkOverflow();
 		drawLines(table);
-		noLoop();
-	}
-
-	int Zona(double x, double y) {
-		int zona = BENT;
-
-		if (x < xMin) zona |= BAL;
-		else if (x > xMax) zona |= JOBB;
-		if (y < yMin) zona |= LENT;
-		else if (y > yMax) zona |= FENT;
-
-		return zona;
-	}
-
-	Line CohenSutherlandSzakaszvago(float x0, float y0, float x1, float y1) {
-		int pont1Zona = Zona(x0, y0);
-		int pont2Zona = Zona(x1, y1);
-		boolean elfogad = false;
-		boolean vege = false;
-
-		do {
-			if ((pont1Zona | pont2Zona) == 0) {
-				// mindket pont bent van
-				elfogad = true;
-				vege = true;
-			} else {
-				if ((pont1Zona & pont2Zona) != 0) {
-					// mindket pont kozos kulso zonaban van (BAL, JOBB, FENT, LENT)
-					// nem fogadjuk el, vege
-					vege = true;
-				} else {
-					float x = 0, y = 0;
-
-					// egy pont biztosan kint van
-					int pontKint = Math.max(pont2Zona, pont1Zona);
-
-					// Metszespontok:
-					// m = (y1 - y0) / (x1 - x0)
-					// x = x0 + (1 / m) * (y[Min/Max] - y0)
-					// y = y0 + m * (x[Min/Max] - x0)
-					if ((pontKint & FENT) != 0) {
-						x = x0 + (x1 - x0) * (yMax - y0) / (y1 - y0);
-						y = yMax;
-					} else if ((pontKint & LENT) != 0) {
-						x = x0 + (x1 - x0) * (yMin - y0) / (y1 - y0);
-						y = yMin;
-					} else if ((pontKint & JOBB) != 0) {
-						y = y0 + (y1 - y0) * (xMax - x0) / (x1 - x0);
-						x = xMax;
-					} else if ((pontKint & BAL) != 0) {
-						y = y0 + (y1 - y0) * (xMin - x0) / (x1 - x0);
-						x = xMin;
-					}
-
-					if (pontKint == pont1Zona) {
-						x0 = x;
-						y0 = y;
-						pont1Zona = Zona(x0, y0);
-					} else {
-						x1 = x;
-						y1 = y;
-						pont2Zona = Zona(x1, y1);
-					}
-				}
-			}
-		}
-		while (!vege);
-
-
-		if (elfogad) {
-			//drawLine(x0, y0, x1, y1);
-			return new Line(x0, y0, x1, y1);
-		}
-		return null;
 	}
 
 	void drawLine(float x1, float y1, float x2, float y2) {
@@ -188,29 +111,7 @@ public class Main extends PApplet {
 			x1 = table.getRow(i + 1).getFloat("x");
 			y1 = table.getRow(i + 1).getFloat("y");
 
-			drawLine(x1, y1, x2, y2); // helyette CohenSutherlandSzakaszvago()
-			/*
-			Line line = CohenSutherlandSzakaszvago(x1, y1, x2, y2);
-			if (translationCutsModel) {
-				if (line != null) {
-					x1 = line.x1;
-					y1 = line.y1;
-					x2 = line.x2;
-					y2 = line.y2;
-
-					table.getRow(i).setFloat("x", x2);
-					table.getRow(i).setFloat("y", y2);
-					table.getRow(i + 1).setFloat("x", x1);
-					table.getRow(i + 1).setFloat("y", y1);
-				} else {
-					table.removeRow(i + 1);
-					table.removeRow(i);
-				}
-			}
-
-			if (line != null)
-				drawLine(line.x1, line.y1, line.x2, line.y2);
-			 */
+			drawLine(x1, y1, x2, y2);
 		}
 	}
 
@@ -228,7 +129,7 @@ public class Main extends PApplet {
 		return transformed;
 	}
 
-	void translate() {
+	void translate(boolean checkOverflow) {
 		countClicks++;
 
 		if (countClicks % 2 == 0) {
@@ -236,15 +137,15 @@ public class Main extends PApplet {
 			transformY = mouseY - transformY;
 			countClicks = 0;
 
-			translate(transformX, transformY);
+			translate(transformX, transformY, checkOverflow);
 		} else {
 			transformX = mouseX;
 			transformY = mouseY;
 		}
 	}
 
-	public void translate(float transformX, float transformY) {
-		float[][] T = tInit;
+	public void translate(float transformX, float transformY, boolean checkOverflow) {
+		float[][] T = new Tinit().matrix;
 		T[0][2] = transformX;
 		T[1][2] = transformY;
 
@@ -252,64 +153,35 @@ public class Main extends PApplet {
 			float[] p = pInit;
 			p[0] = row.getFloat("x");
 			p[1] = row.getFloat("y");
+
 			p = matrixMultiplication(T, p);
 
 			row.setFloat("x", p[0]);
 			row.setFloat("y", p[1]);
 		}
+
+		if (checkOverflow) checkOverflow("translate");
 	}
 
-	void rotate() {
-		float[][] T = tInit;
+	void rotate(boolean checkOverflow) {
+		float[][] T = new Tinit().matrix;
 		T[0][0] = cos(radians(rotateAngle));
 		T[0][1] = -sin(radians(rotateAngle));
 		T[1][0] = sin(radians(rotateAngle));
 		T[1][1] = cos(radians(rotateAngle));
 
 		for (TableRow row : table.rows()) {
+			pInit = new float[]{0, 0, 1};
 			float[] p = pInit;
-			p[0] = row.getFloat("x");
-			p[1] = row.getFloat("y");
+			p[0] = row.getFloat("x") - originX;
+			p[1] = row.getFloat("y") - originY;
 			p = matrixMultiplication(T, p);
 
-			row.setFloat("x", p[0]);
-			row.setFloat("y", p[1]);
+			row.setFloat("x", p[0] + originX);
+			row.setFloat("y", p[1] + originY);
 		}
 
-		/*
-		for (int i = 0; i < table.getRowCount() - 1; i += 2) {
-			float x1 = table.getRow(i).getFloat("x");
-			float y1 = table.getRow(i).getFloat("y");
-			float x2 = table.getRow(i + 1).getFloat("x");
-			float y2 = table.getRow(i + 1).getFloat("y");
-
-			Point point1 = new Point(x1, y1);
-			Point point2 = new Point(x2, y2);
-
-			x1 -= mouseX;
-			y1 -= mouseY;
-
-			x2 -= mouseX;
-			y2 -= mouseY;
-
-			point1.x = x1 * cos(radians(rotateAngle)) - y1 * sin(radians(rotateAngle));
-			point1.y = x1 * sin(radians(rotateAngle)) + y1 * cos(radians(rotateAngle));
-
-			point2.x = x2 * cos(radians(rotateAngle)) - y2 * sin(radians(rotateAngle));
-			point2.y = x2 * sin(radians(rotateAngle)) + y2 * cos(radians(rotateAngle));
-
-			point1.x += mouseX;
-			point1.y += mouseY;
-
-			point2.x += mouseX;
-			point2.y += mouseY;
-
-			table.getRow(i).setFloat("x", point1.x);
-			table.getRow(i).setFloat("y", point1.y);
-			table.getRow(i + 1).setFloat("x", point2.x);
-			table.getRow(i + 1).setFloat("y", point2.y);
-		}
-		 */
+		if (checkOverflow) checkOverflow("translate");
 	}
 
 	void scale() {
@@ -342,12 +214,12 @@ public class Main extends PApplet {
 		}
 	}
 
-	public void checkOverflow() {
-		float minX = 1, minY = 1, maxX = width, maxY = height;
+	public void checkOverflow(String method) {
+		float minX = originX, minY = originY, maxX = width, maxY = height;
+		float deltaX = 0, deltaY = 0;
+		float x, y;
 
 		for (TableRow row : table.rows()) {
-			float x, y;
-
 			x = row.getFloat("x");
 			y = row.getFloat("y");
 
@@ -357,21 +229,29 @@ public class Main extends PApplet {
 			if (y > maxY) maxY = y;
 		}
 
-		if (minX < 1) translate(-minX, 0);
-		if (maxX > width) translate(width - maxX, 0);
+		if (originX - minX > 0) deltaX = originX - minX;
+		if (width - maxX < 0) deltaX = width - maxX;
+		if (originY - minY > 0) deltaY = originY - minY;
+		if (height - maxY < 0) deltaY = height - maxY;
 
-		if (minY < 1) translate(0, -minY);
-		if (maxY > height) translate(0, height - maxY);
+		switch (method) {
+			case "translate": {
+				if (deltaX != 0 || deltaY != 0) translate(deltaX, deltaY, false);
+				break;
+			}
+			case "scale": {
 
+			}
+		}
 	}
 
 	public void mousePressed() {
 		if (translate || rotate || scale) {
 			if (translate) {
-				translate();
+				translate(true);
 			}
 			if (rotate) {
-				rotate();
+				rotate(true);
 			}
 			if (scale) {
 				scale();
@@ -381,7 +261,6 @@ public class Main extends PApplet {
 			newRow.setFloat("x", mouseX);
 			newRow.setFloat("y", mouseY);
 		}
-		redraw();
 	}
 
 	public void keyPressed() {
@@ -414,7 +293,6 @@ public class Main extends PApplet {
 			scale((float) 1.1);
 		else
 			scale((float) (1.0 / 1.1));
-		redraw();
 	}
 
 	public void settings() {
